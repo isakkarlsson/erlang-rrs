67bccd14a7779e901be810f02962ba39f32eafe7
Isak Karlsson <isak-kar@dsv.su.se>
2013-06-26 17:59:58 +0200

    fix: remove rr_server and cleaning up memory

--- src/rf_handler.erl
+++ src/rf_handler.erl
@@ -1,32 +1,3 @@
-%% -module(rf_handler).
-
-%% -export([
-%% 	 init/3,
-%% 	 hello/2,
-%% 	 content_types_provided/2
-%% 	]).
-
-%% content_types_provided(Req, State) ->
-%% 	{[
-%% 		{<<"text/html">>, hello},
-%% 		{<<"application/json">>, hello},
-%% 		{<<"text/plain">>, hello}
-%% 	], Req, State}.
-
-%% init({tcp, http}, _Req, _Opts) ->
-%%     {upgrade, protocol, cowboy_rest}.
-
-%% hello(Req, State) ->
-%%     Body = <<"<html>
-%% <head>
-%% 	<meta charset=\"utf-8\">
-%% 	<title>REST Hello World!</title>
-%% </head>
-%% <body>
-%% 	<p>REST Hello World as HTML!</p>
-%% </body>
-%% </html>">>,
-%%     {Body, Req, State}.
 -module(rf_handler).
 -behaviour(cowboy_websocket_handler).
 
@@ -45,16 +16,14 @@ websocket_init(_TransportName, Req, _Opts) ->
     
     spawn(fun() ->
 		  Csv = csv:binary_reader("../erlang-rr/data/spambase.txt"),
-		  rr_example:init(),
-		  {F, E} = rr_example:load(Csv, 4),
-		  
-		  {Build, Evaluate, _} = rf:new([{no_features, trunc(math:log(length(F))/math:log(2))},
+		  {Features, Examples, ExConf} = rr_example:load(Csv, 4),
+		  {Build, Evaluate, _} = rf:new([{no_features, trunc(math:log(length(Features))/math:log(2))},
 						 {progress, fun (X, Y) -> 
 								    Self ! {msg, io_lib:format("~p of ~p trees done", [X, Y])}
 							    end}]),
-		  rr_eval:cross_validation(F, E, 
+		  rr_eval:cross_validation(Features, Examples, ExConf,
 					   [{build, Build}, 
-					    {evaluate, Evaluate}, 
+					    {evaluate, rf:killer(Evaluate)}, 
 					    {progress, fun (Fold) -> Self ! {msg, io_lib:format("Fold ~p", [Fold])} end}])
 	  end),
     {ok, Req, undefined_state}.
