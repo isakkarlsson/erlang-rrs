2d268cb3dbe68628604b9324c4037dd965a3b1f2
Isak Karlsson <isak-kar@dsv.su.se>
2013-07-23 11:03:07 +0200

    fix: updated ui

--- ui/js/rr.ui.js
+++ ui/js/rr.ui.js
@@ -12,6 +12,49 @@ $(document).ready(function() {
     $(".method").click(slider("#ml-lip"));
     $("#eval-selector").click(slider("#eval-lip"));
 
+    $(".slider").each(function () {
+	$(this).slider({
+	    range: "min",
+	    value: parseInt($(this).attr("default")),
+	    min: parseInt($(this).attr("min")),
+	    max: parseInt($(this).attr("max")),
+	    slide: function( event, ui ) {
+		id = $(this).attr("id") + "-current";
+		$("#" + id).text(ui.value);
+	    }
+	});
+    });
+
+    $(".choice").each(function() {
+	$(this).chosen({
+	    
+	});
+    });
+
+    $(".help").each(function() {
+	var help = $(this);
+	if($(help).attr("show") == "true") {
+	    $(help).append("<sup>?</sup>");
+	}
+	var build = $("#build-information");
+	$(this).hover(function(a) {
+	    var msg = $(help).attr("help");
+	    $(build).prepend(""+
+"<div class='build-information-bubble'>" +
+"  <div class='build-information-bubble-head'>" +
+"    <h3>Information</h3> " +
+"  </div> " + 
+"  <div class='build-information-bubble-contents'>"+
+"   <p> " + msg + "</p> " +
+"  </div> " +
+"</div>");
+	    $("#build-information .build-information-bubble:first").hide().fadeIn("slow");
+	}, function (b) {
+	    $("#build-information .build-information-bubble:first").delay(1000).fadeOut("slow");
+	    
+	});
+    });
+
 });
 
 
